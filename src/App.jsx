3272f1430d68bfc4edc9accd3b65ed0df5f30ae9
roy2pushkar <roy2000pushkar@gmail.com>
2023-12-28 00:38:19 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -73,6 +73,7 @@ const App = () => {
         >
           Search
         </button>
+
         <button
           onClick={toggleTheme}
           className={`px-4 py-2 ${isDarkTheme ? 'bg-gray-600' : 'bg-gray-300'} text-white rounded-md hover:${isDarkTheme ? 'bg-gray-700' : 'bg-gray-400'} focus:outline-none focus:ring focus:border-gray-300 ml-2`}
@@ -80,6 +81,23 @@ const App = () => {
           {isDarkTheme ? 'Bright' : 'Dark'} Theme
         </button>
       </div>
+      {/* Render additional information about the selected university */}
+      {selectedUniversity !== null && (
+        <div className="mt-8 flex flex-col justify-center items-center text-white">
+          <h2 className="text-2xl font-bold mb-2">Selected University Details:</h2>
+          <p><span className="font-semibold">Name:</span> {selectedUniversity.name}</p>
+          <p><span className="font-semibold">Country:</span> {selectedUniversity.country}</p>
+          <p><span className="font-semibold">Code of Country:</span> {selectedUniversity.alpha_two_code}</p>
+          <p><span className="font-semibold">Domains:</span> {selectedUniversity.domains && selectedUniversity.domains.map((domain, index) => (
+            <a key={index} className="cursor-pointer text-blue-700">{domain}</a>
+          ))}</p>
+          <p><span className="font-semibold">Websites:</span> {selectedUniversity.web_pages && selectedUniversity.web_pages.map((webPage, index) => (
+            <a key={index} className="cursor-pointer text-blue-700">{webPage}</a>
+          ))}</p>
+          <p><span className="font-semibold">State:</span> {selectedUniversity.state_province}</p>
+          {/* Add more details as needed */}
+        </div>
+      )}
 
       {universitiesLoading && <p className="text-gray-600">Loading universities...</p>}
       {universitiesError && <p className="text-red-500">{universitiesError}</p>}
@@ -106,23 +124,7 @@ const App = () => {
         </div>
       )}
 
-      {/* Render additional information about the selected university */}
-      {selectedUniversity !== null && (
-        <div className="mt-8 flex flex-col justify-center items-center text-white">
-          <h2 className="text-2xl font-bold mb-2">Selected University Details:</h2>
-          <p><span className="font-semibold">Name:</span> {selectedUniversity.name}</p>
-          <p><span className="font-semibold">Country:</span> {selectedUniversity.country}</p>
-          <p><span className="font-semibold">Code of Country:</span> {selectedUniversity.alpha_two_code}</p>
-          <p><span className="font-semibold">Domains:</span> {selectedUniversity.domains && selectedUniversity.domains.map((domain, index) => (
-            <a key={index} className="cursor-pointer text-blue-700">{domain}</a>
-          ))}</p>
-          <p><span className="font-semibold">Websites:</span> {selectedUniversity.web_pages && selectedUniversity.web_pages.map((webPage, index) => (
-            <a key={index} className="cursor-pointer text-blue-700">{webPage}</a>
-          ))}</p>
-          <p><span className="font-semibold">State:</span> {selectedUniversity.state_province}</p>
-          {/* Add more details as needed */}
-        </div>
-      )}
+      
 
       {console.log("Selected University:", selectedUniversity)}
     </div>
@@ -131,3 +133,4 @@ const App = () => {
 };
 
 export default App;
+
